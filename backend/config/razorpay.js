62e03bc4f7ab07735e9688c3eec63c6c8d44b34c
shubham jha <116482692+shubhamjha2@users.noreply.github.com>
2024-05-31 21:40:24 +0530

    updated

--- backend/config/razorpay.js
+++ backend/config/razorpay.js
@@ -1,5 +1,7 @@
 const Razorpay = require("razorpay");
+const dotenv = require("dotenv");
 
+dotenv.config();
 exports.instance = new Razorpay({
   key_id: process.env.RAZORPAY_KEY,
   key_secret: process.env.RAZORPAY_SECRET,
